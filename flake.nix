aa0ade4a529a8436904d3abf38d7d0cce0539608
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-02-15 19:14:48 +0530

    improvement: Use bleeding version of zig from Mitchell Hashimoto's overlay

--- flake.nix
+++ flake.nix
@@ -3,14 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    zig.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  outputs = { self, nixpkgs, zig }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          zig.overlays.default
+        ];
+      };
     in
     {
       devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = with pkgs; [nixpkgs-fmt zig];
+        buildInputs = with pkgs; [nixpkgs-fmt zigpkgs.master];
       };
   };
 }
