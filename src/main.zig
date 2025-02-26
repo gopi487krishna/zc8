1f57bd8f8494e9106b1f816744111663aa1cefd2
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-02-26 21:36:04 +0530

    chore: Move out stuff from main

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,19 @@
 const std = @import("std");
+const chip8_context = @import("chip8_context.zig");
+const chip8 = @import("chip8.zig");
+const Chip8 = chip8.Chip8;
+
+const testing = std.testing;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    var ctx = chip8_context.initContext();
+    try Chip8.loadRom(&ctx);
+    try Chip8.run(&ctx);
 }
 
 test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    // var list = std.ArrayList(i32).init(std.testing.allocator);
+    // defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    // try list.append(42);
+    // try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
