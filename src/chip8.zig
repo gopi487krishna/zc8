495e908b9e875d1f67f666305a541d6742d12af3
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-03-25 08:40:53 +0530

    chore: Add display driver to chip 8

--- src/chip8.zig
+++ src/chip8.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Chip8Context = @import("chip8_context.zig").Chip8Context;
 const KeyPad = @import("chip8_context.zig").KeyPad;
+const DisplayDriver = @import("sdl_displaydriver.zig").DisplayDriver;
 
 pub const Chip8Error = error{ RomTooLarge, InstructionNotSupported, PcOutOfBounds, StackEmpty };
 
