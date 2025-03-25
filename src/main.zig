769bd9649b9ae7d31a8069bbed9d2d295613e954
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-03-25 08:36:51 +0530

    feat: Accept rom path as an argument instead of hardcoding

--- src/main.zig
+++ src/main.zig
@@ -22,14 +22,22 @@ pub fn main() !void {
     var ctx = try Chip8Context.initContext();
     defer ctx.deinit();
 
-    const test_roms_dir: []const u8 = @import("build_options").test_roms_dir;
-    // const chip8_logo_rom_path = test_roms_dir ++ "/chip8-logo.ch8";
-    // const chip8_logo_rom_path = test_roms_dir ++ "/2-ibm-logo.ch8";
-    const chip8_logo_rom_path = test_roms_dir ++ "/pong.ch8";
-    // const chip8_logo_rom_path = test_roms_dir ++ "/particle_demo.ch8";
+    var args = std.process.args(); 
+    _ = args.skip(); //to skip the zig call
 
-    var chip8 = Chip8{ .ctx = &ctx };
+    var chip8_logo_rom_path: []const u8 = undefined; 
+    if (args.next()) |path| {
+        chip8_logo_rom_path = try std.fs.cwd().realpathAlloc(std.heap.page_allocator, path);
+        std.debug.print("Path : {s}\n", .{chip8_logo_rom_path});
+    }
+    else {
+        std.debug.print("No path provided!\n", .{});
+        std.process.exit(1);
+    }
+
+    var chip8 = Chip8{ .ctx = &ctx};
     try chip8.loadRomFromFile(std.heap.page_allocator, chip8_logo_rom_path);
+    chip8.loadFont();
 
     var start_time: i64 = 0;
     var end_time: i64 = 0;
@@ -39,15 +47,24 @@ pub fn main() !void {
     var quit = false;
 
     while (!quit) {
-
         const delta_time = end_time - start_time;
         start_time = std.time.microTimestamp();
         time_accumulated += delta_time;
         if (time_accumulated > target_frame_time) {
-            try chip8.execute();
             display.clearScreen();
             quit = DisplayDriver.handleEvents(&ctx);
-            display.update(&ctx);
+            var cycles:u8 = 17;
+            while (cycles != 0) {
+                try chip8.execute();
+                cycles -= 1;
+            }
+
+            if (ctx.draw_flag) {
+                display.update(&ctx);
+                ctx.draw_flag = false;
+            }
+            ctx.delay_timer = if (ctx.delay_timer == 0) 0 else ctx.delay_timer - 1;
+            ctx.sound_timer = if (ctx.sound_timer == 0) 0 else ctx.sound_timer - 1;
             time_accumulated = 0;
         }
         end_time = std.time.microTimestamp();
